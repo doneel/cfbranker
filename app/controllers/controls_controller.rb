bec02dd34b494e73fe67a1d9659d18193a1f716f
Daniel O'Neel <danieloneel@gmail.com>
2014-05-07 18:15:38 +0200

    Upload whole zip instead of individual files

--- app/controllers/controls_controller.rb
+++ app/controllers/controls_controller.rb
@@ -1,11 +1,55 @@
 class ControlsController < ApplicationController
 
 	before_filter	:check_for_admin
-
-	before_filter :check_for_year
-	skip_before_action :check_for_year, only: [:updatecsvs]
 	skip_before_action :check_for_admin, only: [:deniedAccess]
+
+	#before_filter :check_for_year
+	#skip_before_action :check_for_year, only: [:updatecsvs]
+
+        before_filter :validate_year
+        skip_before_action :validate_year, only: [:uploadteams, :updatecsvs, :uploadzip]
+
+        def uploadzip
+            check_for_year
+            puts "hello"
+            lister = Proc.new {|file| puts file}
+            Zip::ZipFile.foreach(params[:file].path, &lister)
+
+            Zip::ZipFile.open(params[:file].path) do |zipfile|
+                team_file = zipfile.find_entry("team.csv")
+                game_file = zipfile.find_entry("game.csv")
+                performance_file = zipfile.find_entry("team-game-statistics.csv")
+                conference_file = zipfile.find_entry("conference.csv")
+
+                if team_file == nil || game_file == nil || performance_file == nil || conference_file == nil
+                    flash[:notice] = "Failed to open all files in #{zipfile.name}"
+		    redirect_to '/controls/updatecsvs'
+                end
+
+                
+                team_filename = zipfile.name + '_' + team_file.to_s
+                team_file.extract(team_filename)
+                num = Team.import(File.new(team_filename), params[:year])
+
+                game_filename = zipfile.name + '_' + game_file.to_s
+                game_file.extract(game_filename)
+                num = Game.import(File.new(game_filename), params[:year])
+
+                performance_filename = zipfile.name + '_' + performance_file.to_s
+                performance_file.extract(performance_filename)
+                num = Performance.import(File.new(performance_filename), params[:year])
+
+                conference_filename = zipfile.name + '_' + conference_file.to_s
+                conference_file.extract(conference_filename)
+                num = Conference.import(File.new(conference_filename), params[:year])
+                flash[:notice] = "Succesfully extracted #{zipfile.name}"
+		redirect_to '/controls/updatecsvs'
+            end
+        end
+
+
 	def uploadteams
+                check_for_year
 		num = Team.import(params[:file], params[:year])
 		flash[:notice] = "Succesfully added #{num} teams"
 		redirect_to '/controls/updatecsvs'
@@ -56,8 +100,16 @@ class ControlsController < ApplicationController
 
 	private
 
+        def validate_year
+            if params[:year] != nil && !Season.exists?(params[:year]) 
+                flash[:error] = "That year doesn't exist."
+		redirect_to '/controls/updatecsvs'
+            end
+        end
+
+
 	def check_for_year
-		if Season.exists?(params[:year]) == nil
+		if params[:year] != nil && Season.exists?(params[:year]) == nil
 			y = Season.new
 			y.year = params[:year]
 			y.save
